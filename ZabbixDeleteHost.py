6c1548a2a14e189d25c1946e668ebfe328dd13d5
fenweak <karldavidbelli@gmail.com>
2015-02-23 17:56:59 +0100

    fix

--- ZabbixDeleteHost.py
+++ ZabbixDeleteHost.py
@@ -1,9 +1,12 @@
 from zabbix_api import ZabbixAPI
-import ConfigParser
+import configparser
 
-#Set server/login
-config = ConfigParser.RawConfigParser()
+#Get server/login
+config = configparser.RawConfigParser()
 config.readfp(open("zabbixinfo.conf"))
+server = config.get('credentials','server')
+login = config.get('credentials','login')
+password = config.get('credentials','password')
 
 zapi = ZabbixAPI(server="server")
 zapi.login("login", "password")
