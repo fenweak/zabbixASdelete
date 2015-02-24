43447ad4afb82dbda47fd6a01bc206ef718ef805
fenweak <karldavidbelli@gmail.com>
2015-02-24 18:05:08 +0100

    add delete with hostname

--- ZabbixDeleteHost.py
+++ ZabbixDeleteHost.py
@@ -8,5 +8,14 @@ server = config.get('credentials','server')
 login = config.get('credentials','login')
 password = config.get('credentials','password')
 
-zapi = ZabbixAPI(server="server")
-zapi.login("login", "password")
+zapi = ZabbixAPI(server=server)
+zapi.login(login, password)
+
+# get the hostId from hostName
+def getID(host): 
+    hostId = zapi.host.get({"filter": {"host": host}})[0]["hostid"]
+    return zapi.item.getObjects({'hostid': hostId})
+
+#Delete !
+idtodelete = getID(hostname)
+zapi.delete.host( [{"hostid" : int([idtodelete]) }] )
